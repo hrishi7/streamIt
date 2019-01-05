4b44e08ae6babee29e9ef2cd76b8ab223e901331
Hrishikesh Baidya <hrishikeshbaidya7@gmail.com>
2019-01-05 22:49:26 +0530

    adding radio button for adult section and adding search button

--- js/movie.js
+++ js/movie.js
@@ -1,75 +1,100 @@
+const notAvailable = "N/A";
 
-const notAvailable = 'N/A';
-
-function movieSelected(id){
-  sessionStorage.setItem('movieId', id);
-  window.location = 'singleMovie.html';
+function movieSelected(id) {
+  sessionStorage.setItem("movieId", id);
+  window.location = "singleMovie.html";
   return false;
 }
 
+function getMovie() {
+  let movieId = sessionStorage.getItem("movieId");
 
-
-function getMovie(){
-  let movieId = sessionStorage.getItem('movieId');
-
-  axios.get('https://api.themoviedb.org/3/movie/'+movieId+'?api_key=ca5d667528ca51e527d9e4f7830d97d2&include_adult=false')
-  .then((response) =>{
-    console.log(response);
-    let movie = response.data;
-    let output =`
+  axios
+    .get(
+      "https://api.themoviedb.org/3/movie/" +
+        movieId +
+        "?api_key=ca5d667528ca51e527d9e4f7830d97d2&include_adult=false"
+    )
+    .then(response => {
+      console.log(response);
+      let movie = response.data;
+      let output = `
       <div class="row">
 
-        ${!movie.poster_path ?
-
-      `
+        ${
+          !movie.poster_path
+            ? `
       <div class="col-md-4">
          <img class="img-fluid" src="img/no-poster.gif" alt="${movie.title}">
        </div>
 
-        `:`
+        `
+            : `
           <div class="col-md-4">
-            <img class="img-fluid" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="${movie.title}">
+            <img class="img-fluid" src="https://image.tmdb.org/t/p/w500/${
+              movie.poster_path
+            }" alt="${movie.title}">
           </div>
           `
-      }
+        }
         <div class="col-md-8">
           <h2>${movie.original_title}</h2>
             <ul class="list-group">
-              ${!movie.release_date?
-                `
+              ${
+                !movie.release_date
+                  ? `
                 <li class="list-group-item"><strong>Released On:</strong>${notAvailable}</li>
-                `:`
-              <li class="list-group-item"><strong>Released On:</strong> ${movie.release_date}</li>
+                `
+                  : `
+              <li class="list-group-item"><strong>Released On:</strong> ${
+                movie.release_date
+              }</li>
               `
-            }
-            ${!movie.vote_average?
-              `
+              }
+            ${
+              !movie.vote_average
+                ? `
               <li class="list-group-item"><strong>Rated:</strong>${notAvailable}</li>
-              `:`
-            <li class="list-group-item"><strong>Rated:</strong> ${movie.vote_average}</li>
+              `
+                : `
+            <li class="list-group-item"><strong>Rated:</strong> ${
+              movie.vote_average
+            }</li>
             `
-          }
-          ${!movie.tagline?
-            `
+            }
+          ${
+            !movie.tagline
+              ? `
             <li class="list-group-item"><strong>Tagline:</strong>${notAvailable}</li>
-            `:`
-          <li class="list-group-item"><strong>Tagline:</strong> ${movie.tagline}</li>
+            `
+              : `
+          <li class="list-group-item"><strong>Tagline:</strong> ${
+            movie.tagline
+          }</li>
           `
-        }
-        ${!movie.production_companies[0]?
-          `
+          }
+        ${
+          !movie.production_companies[0]
+            ? `
           <li class="list-group-item"><strong>production Companies:</strong>${notAvailable}</li>
-          `:`
-        <li class="list-group-item"><strong>production Companies:</strong> ${movie.production_companies[0].name}</li>
+          `
+            : `
+        <li class="list-group-item"><strong>production Companies:</strong> ${
+          movie.production_companies[0].name
+        }</li>
         `
-      }
-      ${!movie.production_countries[0]?
-        `
+        }
+      ${
+        !movie.production_countries[0]
+          ? `
         <li class="list-group-item"><strong>production Country:</strong>${notAvailable}</li>
-        `:`
-      <li class="list-group-item"><strong>production Country:</strong> ${movie.production_countries[0].name}</li>
+        `
+          : `
+      <li class="list-group-item"><strong>production Country:</strong> ${
+        movie.production_countries[0].name
+      }</li>
       `
-    }
+      }
 
             </ul>
         </div>
@@ -77,31 +102,36 @@ function getMovie(){
       <div class="row">
         <div class="well">
           <h3>Plot</h3>
-            ${!movie.overview?
-              `
+            ${
+              !movie.overview
+                ? `
             ${notAvailable}
-              `:`
+              `
+                : `
            ${movie.overview}
             `
-          }
+            }
           <hr>
-            <a href ="https://imdb.com/title/${movie.imdb_id}" target ="_blank" class="btn btn-primary">View Imdb</a>
+            <a href ="https://imdb.com/title/${
+              movie.imdb_id
+            }" target ="_blank" class="btn btn-primary">View Imdb</a>
             <a href="movies.html" clas="btn btn-default">Go back to search</a>
         </div>
       </div>
       <br>
       <div class="row">
         <div class="conatiner embed-responsive embed-responsive-4by3">
-        <iframe src="https://videospider.in/getvideo?key=Gbk0l1UNDHK7Uz9S&video_id=${movie.imdb_id}" style=" width: 100%; height: 100%" allowfullscreen="true" scrolling="no" class="embed-responsive-item"></iframe>
+        <iframe src="https://videospider.in/getvideo?key=Gbk0l1UNDHK7Uz9S&video_id=${
+          movie.imdb_id
+        }" style=" width: 100%; height: 100%" allowfullscreen="true" scrolling="no" class="embed-responsive-item" ></iframe>
         </div>
       </div>
 
 
     `;
-    $('#movie').html(output);
-  })
-  .catch((err) =>{
-    console.log(err);
-  });
-
+      $("#movie").html(output);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 }
--- js/searchMovie.js
+++ js/searchMovie.js
@@ -1,59 +1,70 @@
-
-
 let adultFlag = false;
 $(document).ready(() => {
-  $('#searchForm').on('submit', (e) => {
-    let searchText = $('#searchText').val();
-    adultFlag = $('#adult').val();
+  $("#searchForm").on("submit", e => {
+    let searchText = $("#searchText").val();
+    //adultFlag = $('#adult').value();
+    adultFlag = $("input[name=adult]:checked").val();
     getMovies(searchText);
     e.preventDefault();
-
   });
 });
 
+function getMovies(searchText) {
+  axios
+    .get(
+      "https://api.themoviedb.org/3/search/movie?api_key=ca5d667528ca51e527d9e4f7830d97d2&language=en-US&query=" +
+        searchText +
+        "&page=1&include_adult=" +
+        adultFlag
+    )
+    .then(response => {
+      let movies = response.data.results;
 
-function getMovies(searchText){
-  axios.get('https://api.themoviedb.org/3/search/movie?api_key=ca5d667528ca51e527d9e4f7830d97d2&language=en-US&query='+searchText+'&page=1&include_adult='+adultFlag)
-  .then((response) =>{
-    let movies = response.data.results;
-    console.log(response);
-
-    let output ='';
-    if(movies.length>0){
-    $.each(movies, (index, movie) =>{
-      output += `
+      let output = "";
+      if (movies.length > 0) {
+        $.each(movies, (index, movie) => {
+          output += `
       <div class="col-md-4 col-sm-6 portfolio-item">
-        ${!movie.poster_path ?
-
-      `
-      <a class="portfolio-link" data-toggle="modal" href="#" onclick="movieSelected('${movie.id}')">
+        ${
+          !movie.poster_path
+            ? `
+      <a class="portfolio-link" data-toggle="modal" href="#" onclick="movieSelected('${
+        movie.id
+      }')">
          <img class="img-fluid" src="img/no-poster.gif" alt="${movie.title}">
        </a>
 
-        `:`
-        <a class="portfolio-link" data-toggle="modal" href="#" onclick="movieSelected('${movie.id}')">
-            <img class="img-fluid" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="${movie.title}">
+        `
+            : `
+        <a class="portfolio-link" data-toggle="modal" href="#" onclick="movieSelected('${
+          movie.id
+        }')">
+            <img class="img-fluid" src="https://image.tmdb.org/t/p/w500/${
+              movie.poster_path
+            }" alt="${movie.title}">
           </a>
           `
-      }
+        }
         <div class="portfolio-caption">
           <h4 style="color:black">${movie.title}</h4>
           Released On<p class="text-muted">${movie.release_date}</p>
-        <a onclick="movieSelected('${movie.id}')" class="btn btn-primary" href="#">Movie Details</a>
+        <a onclick="movieSelected('${
+          movie.id
+        }')" class="btn btn-primary" href="#">Movie Details</a>
 
         </div>
       </div>
 
       `;
+        });
+        $("#movies").html(output);
+      } else {
+        output =
+          '<h2 class="text-center">No Movie Available with this Name!! Please Try Another Name</h2>';
+        $("#movies").html(output);
+      }
+    })
+    .catch(err => {
+      console.log(err);
     });
-    $('#movies').html(output);
-  }
-  else {
-    output ='<h2 class="text-center">No Movie Available with this Name!! Please Try Another Name</h2>'
-    $('#movies').html(output);
-  }
-  })
-  .catch((err) =>{
-    console.log(err);
-  });
 }
