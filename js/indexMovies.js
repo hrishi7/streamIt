d318e3ec718bdbfa11ab2a302129c27e40c50389
Abba Danmusa <danmusaabdullahi@gmail.com>
2023-02-01 01:07:19 +0100

    movies list display resized

--- js/indexMovies.js
+++ js/indexMovies.js
@@ -1,13 +1,12 @@
-
-$( document ).ready(function() {
-  axios.get('https://api.themoviedb.org/3/discover/movie?api_key=ca5d667528ca51e527d9e4f7830d97d2&language=en-US&sort_by=popularity.desc&include_adult=false')
-  .then((response) =>{
-    let movies = response.data.results;
-    console.log(response);
-    let output ='';
-    $.each(movies, (index, movie) =>{
-      output += `
-      <div class="col-md-4 col-sm-6 portfolio-item">
+$(document).ready(function() {
+    axios.get('https://api.themoviedb.org/3/discover/movie?api_key=ca5d667528ca51e527d9e4f7830d97d2&language=en-US&sort_by=popularity.desc&include_adult=false')
+        .then((response) => {
+            let movies = response.data.results;
+            console.log(response);
+            let output = '';
+            $.each(movies, (index, movie) => {
+                output += `
+      <div class="col-md-2 col-sm-4 portfolio-item">
         <a class="portfolio-link" data-toggle="modal" href="#" onclick="movieSelected('${movie.id}')">
           <img class="img-fluid" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="${movie.title}">
         </a>
@@ -19,21 +18,22 @@ $( document ).ready(function() {
       </div>
 
       `;
-    });
-    $('#home-movies').html(output);
-  })
-  .catch((err) =>{
-    console.log(err);
-  });
+            });
+            $('#home-movies').html(output);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 });
 
 
 //function to display navbar background-color on scroll 
-window.onscroll = function(){scrollFunction()}
-function scrollFunction(){
-  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10){
-    document.getElementById('mainNav').style.backgroundColor = '#323033';
-  }else{
-    document.getElementById('mainNav').style.backgroundColor = 'transparent';
-  }
+window.onscroll = function() { scrollFunction() }
+
+function scrollFunction() {
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+        document.getElementById('mainNav').style.backgroundColor = '#323033';
+    } else {
+        document.getElementById('mainNav').style.backgroundColor = 'transparent';
+    }
 }
