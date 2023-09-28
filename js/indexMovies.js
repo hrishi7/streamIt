57176811a675737d17e6b4996c94dc40c7b70a07
briviamoon <briviamoon@gmail.com>
2023-09-28 22:16:20 +0300

    Added boxicons resources for optimal icons. Fixed syntax issues on movie API in indexMovies.js added background colour of body to base theme color of nav bar.

--- js/indexMovies.js
+++ js/indexMovies.js
@@ -1,30 +1,29 @@
 $(document).ready(function () {
   axios
     .get(
-    'https://api.themoviedb.org/3/discover/movie?api_key=ca5d667528ca51e527d9e4f7830d97d2&language=en-US&sort_by=popularity.desc&include_adult=false'
+      'https://api.themoviedb.org/3/discover/movie?api_key=ca5d667528ca51e527d9e4f7830d97d2&language=en-US&sort_by=popularity.desc&include_adult=false'
     )
     .then((response) => {
       let movies = response.data.results;
       let output = "";
-      $.each(movies, (index, movie) => {
-        output += `
-      <div class="col-md-2 col-sm-4 portfolio-item">
-      let output = '';
-      $.each(movies, (index, movie) => {
-        output += `
-      <div class="col-md-4 col-sm-6 portfolio-item">
-        <a class="portfolio-link" data-toggle="modal" href="#" onclick="movieSelected('${movie.id}')">
-          <img class="img-fluid" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="${movie.title}">
-        </a>
-        <div class="portfolio-caption">
-          <h4>${movie.title}</h4>
-          Released On<p class="text-muted">${movie.release_date}</p>
-        <a onclick="movieSelected('${movie.id}')" class="btn btn-primary" href="#">Movie Details</a>
-        </div>
-      </div>
 
-      `;
+      $.each(movies, (index, movie) => {
+        output += `
+          <div class="col-md-2 col-sm-4 portfolio-item">
+            <div class="col-md-4 col-sm-6 portfolio-item">
+              <a class="portfolio-link" data-toggle="modal" href="#" onclick="movieSelected('${movie.id}')">
+                <img class="img-fluid" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="${movie.title}" />
+              </a>
+              <div class="portfolio-caption">
+                <h4>${movie.title}</h4>
+                <p class="text-muted">Released On ${movie.release_date}</p>
+                <a onclick="movieSelected('${movie.id}')" class="btn btn-primary" href="#">Movie Details</a>
+              </div>
+            </div>
+          </div>
+        `;
       });
+
       $("#home-movies").html(output);
     })
     .catch((err) => {
@@ -32,10 +31,11 @@ $(document).ready(function () {
     });
 });
 
-//function to display navbar background-color on scroll
+// Function to display navbar background-color on scroll
 window.onscroll = function () {
   scrollFunction();
 };
+
 function scrollFunction() {
   if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
     document.getElementById("mainNav").style.backgroundColor = "#323033";
@@ -43,29 +43,9 @@ function scrollFunction() {
     document.getElementById("mainNav").style.backgroundColor = "transparent";
   }
 }
-      $('#home-movies').html(output);
 
-      //function to display navbar background-color on scroll
-      $(window).scroll(function () {
-        if (
-          document.body.scrollTop > 20 ||
-          document.documentElement.scrollTop > 20
-        ) {
-          $('#mainNav').css({ 'background-color': '#323033' });
-          $('#gtp').css({ transform: 'scale(100%)' });
-        } else {
-          $('#mainNav').css({ 'background-color': 'transparent' });
-          $('#gtp').css({ transform: 'scale(0)' });
-        }
-      });
-
-      //function to scroll to the top when the user clicks the "goToTop" button
-      $('#gtp').click(function () {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+// Function to scroll to the top when the user clicks the "goToTop" button
+$('#gtp').click(function () {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 });
